89baa13195da8bc1456ff2459c0f04d7634fe950
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2020-03-16 21:58:22 +0100

    dev

---
+++ FileJoin/FileEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace FileJoin
+{
+    public class FileEntry
+    {
+        public string FullPath { get; set; }
+        public string FileName => Path.GetFileName(FullPath);
+        public string FilePath => Path.GetDirectoryName(FullPath);
+    }
+}
---
+++ FileJoin/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using JToolbox.Desktop.Dialogs;
+using JToolbox.WPF.Core.Base;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace FileJoin
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string status;
+        private bool useSeparator;
+        private ObservableCollection<FileEntry> fileEntries = new ObservableCollection<FileEntry>();
+        private readonly IDialogsService dialogsService;
+
+        public MainViewModel()
+        {
+            dialogsService = new DialogsService();
+
+            FileEntries.Add(new FileEntry
+            {
+                FullPath = @"e:\TEMP\file1.pdf"
+            });
+            FileEntries.Add(new FileEntry
+            {
+                FullPath = @"e:\TEMP\file2.reg"
+            });
+            FileEntries.Add(new FileEntry
+            {
+                FullPath = @"e:\TEMP\file3.pdf"
+            });
+        }
+
+        public RelayCommand MergeCommand => new RelayCommand(() =>
+        {
+            Status = "Status";
+        });
+
+        public ObservableCollection<FileEntry> FileEntries
+        {
+            get => fileEntries;
+            set => Set(ref fileEntries, value);
+        }
+
+        public string Status
+        {
+            get => status;
+            set => Set(ref status, value);
+        }
+
+        public bool UseSeparator
+        {
+            get => useSeparator;
+            set => Set(ref useSeparator, value);
+        }
+    }
+}
